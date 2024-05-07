b16bea472a590f53ffa10acca27926ac89c18a82
HulkD <86944744+RobertLUNISA@users.noreply.github.com>
2024-05-07 17:50:55 +0930

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -1,12 +1,9 @@
 import boto3
 import os
 from dotenv import load_dotenv
-from decimal import Decimal
-
 
 # Load environment variables
 load_dotenv()
-
 
 def extract_and_upload_metadata(data, algorithm_name, bucket_name, file_path, s3_link, target_variable):
     """Extract metadata from the dataset and upload it to DynamoDB."""
@@ -20,8 +17,8 @@ def extract_and_upload_metadata(data, algorithm_name, bucket_name, file_path, s3
     topic = input("Enter the topic (1 for Health, 2 for Finance, 3 for Entertainment, 4 for Technology, 5 for Education): ")
     topics = {1: "Health", 2: "Finance", 3: "Entertainment", 4: "Technology", 5: "Education"}
     source_link = input("Enter the source link: ")
-    size_in_mb = Decimal(f"{os.path.getsize(file_path) / (1024 * 1024):.2f}")  # Convert size to Decimal
-
+    size_in_kb = os.path.getsize(file_path) // 1024 
+    
     try:
         response = table.put_item(
             Item={
@@ -30,12 +27,14 @@ def extract_and_upload_metadata(data, algorithm_name, bucket_name, file_path, s3
                 'Topic': topics[int(topic)],
                 'Number of Instances': int(data.shape[0]),
                 'Number of Features': int(data.shape[1]),
-                'Size in MB': size_in_mb,
+                'Size in KB': size_in_kb,
                 'Source Link': source_link,
                 'Download Link': s3_link,
                 'Target Variable': target_variable
             }
         )
+
         print("Metadata uploaded successfully to DynamoDB.")
     except Exception as e:
         print(f"Failed to upload metadata: {e}")
+
