cd8eeb12be4179abd788cd0d1f2569e6563b7654
RobertLUNISA <lamtt008@mymail.unisa.edu.au>
2024-05-15 13:56:13 +0930

    Modulate Frontend code, added user_ath.py, updated name Dataset_Website.py to website.py

--- website.py
+++ website.py
@@ -1,67 +1,8 @@
 import streamlit as st
 import pandas as pd
-import boto3
 import logging
 from web_implementation import generate_presigned_url, fetch_dataset_metadata, make_dataset_unclean
-from dotenv import load_dotenv
-import os
-
-# Load environment variables from .env file
-load_dotenv()
-
-# Setup logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# Load AWS Cognito configuration from environment variables
-COGNITO_USER_POOL_ID = os.getenv("COGNITO_USER_POOL_ID")
-COGNITO_APP_CLIENT_ID = os.getenv("COGNITO_APP_CLIENT_ID")
-COGNITO_IDENTITY_POOL_ID = os.getenv("COGNITO_IDENTITY_POOL_ID")
-AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
-
-cognito_client = boto3.client('cognito-idp', region_name=AWS_DEFAULT_REGION)
-identity_client = boto3.client('cognito-identity', region_name=AWS_DEFAULT_REGION)
-
-# Cognito signup and signin functions
-def signup_user(email, password):
-    try:
-        response = cognito_client.sign_up(
-            ClientId=COGNITO_APP_CLIENT_ID,
-            Username=email,
-            Password=password,
-            UserAttributes=[
-                {'Name': 'email', 'Value': email}
-            ]
-        )
-        cognito_client.admin_confirm_sign_up(
-            UserPoolId=COGNITO_USER_POOL_ID,
-            Username=email
-        )
-        return response
-    except Exception as e:
-        logging.error(f"Error signing up: {e}")
-        return None
-
-def authenticate_user(email, password):
-    try:
-        response = cognito_client.initiate_auth(
-            ClientId=COGNITO_APP_CLIENT_ID,
-            AuthFlow='USER_PASSWORD_AUTH',
-            AuthParameters={
-                'USERNAME': email,
-                'PASSWORD': password
-            }
-        )
-        logging.info(f"Authentication response: {response}")
-        return response
-    except cognito_client.exceptions.NotAuthorizedException:
-        logging.error("The username or password is incorrect")
-        return None
-    except cognito_client.exceptions.UserNotConfirmedException:
-        logging.error("User is not confirmed")
-        return None
-    except Exception as e:
-        logging.error(f"Error authenticating: {e}")
-        return None
+from user import signup_user, authenticate_user
 
 # Custom styles
 st.markdown("""
